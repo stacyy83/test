5ceed6dfff5e37f79f8d93fcac3be3a58ed80d89
stacyy83 <stacy@capsulecorporation.cc>
2017-06-22 11:23:48 +0800

    0621 更新 Route+View

--- test/app/Http/routes.php
+++ test/app/Http/routes.php
@@ -1,5 +1,7 @@
 <?php
 
+/*Route::pattern('id','[0-9]+');*/
+
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -11,6 +13,20 @@
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', ['as' => 'test.test', function(){
+	return 'Hello';
+}]);
+*/
+
+Route::get('/',['as' => 'home.index', function(){
+	return view('index');
+}]);
+
+Route::get('about.html', ['as' => 'about.index' ,function(){
+	return view('about');
+}]);
